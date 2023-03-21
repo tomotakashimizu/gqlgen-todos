4aedfb5daebedf6d329fc4aeab750707c2b864ac
tomotakashimizu <tomotaka.developer@gmail.com>
2023-03-21 22:58:10 +0900

    obj *model.Todo から User の情報を取得して ID, name を返す
    しかし、これだと一度 User の情報を全て取得しているため、DBから取得する負荷の軽減ができていない。
    （またエラーは起きていないが、DB取得のコードを書こうとした時に現実的でないかも）
    そのため、今回の場合は User ID の情報を obj *model.Todo に持たせて、1つのレコードのみからデータ取得した方が良い。
    （Don’t eagerly fetch the user の本質とは関係なく、関係あるのは下記）
    
    また resolver を作成する理由は、フロントエンドのクエリで呼び出された場合のみバックエンドでDBから値を取得し、負荷軽減するためである。
    フロントエンドのクエリで指定されない場合、指定されなかった変数のresolverはバックエンドで呼ばれないため、無駄なDB取得処理をしなくて済む。
    resolver を作成していなかったら、フロントエンドのクエリで指定されない変数もバックエンドでDBから取得する処理を一律で書くため無駄な負荷がかかる。

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -32,7 +32,7 @@ func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
 
 // User is the resolver for the user field.
 func (r *todoResolver) User(ctx context.Context, obj *model.Todo) (*model.User, error) {
-	panic(fmt.Errorf("not implemented: User - user"))
+	return &model.User{ID: obj.User.ID, Name: obj.User.Name}, nil
 }
 
 // Mutation returns MutationResolver implementation.
